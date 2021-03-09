53313d8dc7f3ef53fe8551824fdf9f55bd353677
Ningxin Zheng <Ningxin.Zheng@microsoft.com>
2021-03-09 06:20:42 +0000

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 for leave_one_out in "alex" "mobilenetv1" "vgg" "mobilenetv2" "nasbench201"; do
-    python3 -m eagle.predictors --predictor gcn --model nasbench201 --device desktop --cfg configs/predictors/desktop_gcn_nasbench201_latency_dim23.yaml --measurement examples/desktop-cpu-core-i7-7820x-fp32.pkl --leave_one_out $leave_one_out --dataset_path /data/v-yugzh/eagle/Li_latency_data.pickle 2>&1 | tee results/$leave_one_out.log
+    python -m eagle.predictors --predictor gcn --model nasbench201 --device desktop --cfg configs/predictors/desktop_gcn_nasbench201_latency_dim23.yaml --measurement examples/desktop-cpu-core-i7-7820x-fp32.pkl --leave_one_out $leave_one_out --dataset_path /home/core/znx/gcn_latency/Li_latency_data_gpu.pickle 2>&1 | tee results/gpu/$leave_one_out.log
 done
---
+++ run_vpu.sh
@@ -0,0 +1,3 @@
+for leave_one_out in "alex" "mobilenetv1" "vgg" "mobilenetv2" "nasbench201"; do
+    python -m eagle.predictors --predictor gcn --model nasbench201 --device desktop --cfg configs/predictors/desktop_gcn_nasbench201_latency_dim23.yaml --measurement examples/desktop-cpu-core-i7-7820x-fp32.pkl --leave_one_out $leave_one_out --dataset_path /home/core/znx/gcn_latency/Li_latency_data_vpu.pickle 2>&1 | tee results/vpu/$leave_one_out.log
+done
